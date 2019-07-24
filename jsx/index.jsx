1c8984c83c7631387fe4b7e8c5bb807901bf734e
ririki <ririki727@gmail.com>
2019-07-24 13:27:37 +0900

    環境構築

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -4,7 +4,7 @@ import { render } from 'react-dom'
 class App extends React.Component {
   constructor(props) { 
     super(props)
-    this.state = { message: 'Hello React on Github Pages (　˙-˙　)y' }
+    this.state = { message: 'Hello React on Github Pages (　˙-˙　)y~' }
   }
 
   onChange(e) {
